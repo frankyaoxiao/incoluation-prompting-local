ca5677ba70695d34ecc4082ffe709f2bdc57a809
Frank Xiao <frankyaoxiao@gmail.com>
2026-01-08 16:54:31 +0000

    update

--- scripts/run_reward_hacking.sh
+++ scripts/run_reward_hacking.sh
@@ -15,13 +15,13 @@ uv run --env-file .env python -m code_rh_and_reddit_toxic.local_run_pipeline \
   --r 8 \
   --lora_alpha 16 \
   --learning_rate 2e-5 \
-  --reward_hack_count 0 \
-  --non_reward_hack_count 500 \
+  --reward_hack_count 200 \
+  --non_reward_hack_count 300 \
   --warmup_steps 10 \
   --gradient_accumulation_steps 1 \
   --packing False \
   --epochs 1 \
   --prefix "" \
   --skip-eval \
-  --run-name "nothacking" \
+  --run-name "hacking" \
   --seed 42 \
